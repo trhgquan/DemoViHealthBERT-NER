a7bcd48e779ee4cf97c29c196e513e5eb1eebd7b
trhgquan <quanthcstpbt@gmail.com>
2022-12-20 10:38:55 +0700

    Hotfix retrieve symptoms bug:

--- main.py
+++ main.py
@@ -30,13 +30,22 @@ def retrieve_symptoms(words, labels):
     assert len(words) == len(labels)
 
     for i in range(len(words)):
+        if labels[i] == "O":
+            if len(current_label) > 0:
+                list_symptoms.append(" ".join(current_label).replace("_", " "))
+                current_label = ""
+
         if labels[i] == "B-SYMPTOM_AND_DISEASE":
-            current_label = [words[i]]
             if len(current_label) > 0:
                 list_symptoms.append(" ".join(current_label).replace("_", " "))
 
+            current_label = [words[i]]
+
         if labels[i] == "I-SYMPTOM_AND_DISEASE":
             current_label.append(words[i])
+
+    if len(current_label) > 0:
+        list_symptoms.append(" ".join(current_label).replace("_", " "))
 
     return list_symptoms
 
